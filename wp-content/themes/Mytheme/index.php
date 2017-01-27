7edd8acc1571c88ed055d8b33336c9fc9b678601
Pintu Soliya <lanetteam.pintus@gmail.com>
2017-01-27 16:23:27 +0530

    Create Custom Table with plugin

--- wp-content/themes/Mytheme/index.php
+++ wp-content/themes/Mytheme/index.php
@@ -1,6 +1,23 @@
-<?php
-get_header();
-get_sidebar();
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+<div>
+<?php get_header(); ?>
+</div>
+<div>
+	<div>
+	<?php echo "<h1>Main Content</h1>"; ?>
+	</div>
+	<div>
+	<?php  get_sidebar(); ?>
+	</div>
+</div>
+<div>
+<?php get_footer(); ?>
+</div>
+</body>
+</html>
 
-get_footer();
-?>
