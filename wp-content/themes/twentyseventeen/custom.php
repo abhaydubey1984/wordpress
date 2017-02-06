7fe78d56c977507771c8e5c222d28f4ff5fa8ede
Pintu Soliya <lanetteam.pintus@gmail.com>
2017-02-06 17:53:32 +0530

    pdf generating

--- wp-content/themes/twentyseventeen/custom.php
+++ wp-content/themes/twentyseventeen/custom.php
@@ -1,73 +1,26 @@
 <?php
-/*
-Template Name:custom
-*/
-
+//Template Name:Custom_Template
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	
+	<title></title>
 </head>
 <body>
-<?php
-
-if(isset($_REQUEST['submit']))
-{
-	
-	  global $name,$address,$city,$phone;
-	  $name = $_REQUEST['name'];
-	  $address = $_REQUEST['address'];
-	  $city = $_REQUEST['city'];
-	  $phone = $_REQUEST['phone'];
-	  global $wpdb;
-	  $table_name = $wpdb->prefix."contact";
-	  $wpdb->insert($table_name, array('name' => $name, 'address' => $address,'city'=>$city,'phone'=>$phone)); 
-	//header('location:localhost/wordpress');
-}
-?>
 <form method="post">
-	<table>
-		<tr>
-		<td>
-		Name
-		</td>
-		<td>
-		<input type="text" name="name">
-		</td>
-		</tr>
-		<tr>
-		<td>
-		Address
-		</td>
-		<td>
-		<input type="text" name="address">
-		</td>
-		</tr>
-		<tr>
-		<td>
-		City
-		</td>
-		<td>
-		<input type="text" name="city">
-		</td>
-		</tr>
-		<tr>
-		<td>
-		Phone Number
-		</td>
-		<td>
-		<input type="text" name="phone">
-		</td>
-		</tr>
-		<tr>
-		<td>
-		<input type="submit" name="submit" value="Submit">
-		</td>
-		</tr>
-
-	</table>
+<input type="text" name="name">
+<input type="submit" name="submit" value="pdf" />
 </form>
 </body>
 </html>
+<?php
+if(isset($_REQUEST['submit']))
+{
+	include("MPDF57/mpdf.php");
+	$mpdf=new mPDF('c','A4','','' , 0 , 0 , 0 , 0 , 0 , 0); 
+	$mpdf->SetDisplayMode('fullpage');
+	$mpdf->list_indent_first_level = 0;
+	$mpdf->WriteHTML(file_get_contents("http://localhost/wordpress_Demo/pdf-2/"));
+	$mpdf->Output("plan.pdf","D");	
+}
+?>
